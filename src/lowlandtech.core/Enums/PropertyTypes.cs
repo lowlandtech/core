49b674e7b0050df846132d63ef5b5d522fcc74bc
wendellmva <wendellmva@lowlandtech.com>
2024-12-11 01:17:06 +0100

    chore: add property type enum

---
+++ src/lowlandtech.core/Enums/PropertyTypes.cs
@@ -0,0 +1,67 @@
+﻿namespace LowlandTech.Core.Enums;
+
+/// <summary>
+/// Represents the property type.
+/// </summary>
+public enum PropertyTypes
+{
+    /// <summary>
+    /// Gets the string as default property type.
+    /// </summary>
+    String = 0,
+
+    /// <summary>
+    /// Gets the int as property type.
+    /// </summary>
+    Int = 1,
+
+    /// <summary>
+    /// Gets the bool as property type.
+    /// </summary>
+    Bool = 2,
+
+    /// <summary>
+    /// Gets the datetime as property type.
+    /// </summary>
+    DateTime = 3,
+
+    /// <summary>
+    /// Gets the money type as property type.
+    /// </summary>
+    Money = 4,
+
+    /// <summary>
+    /// Gets decimal as property type.
+    /// </summary>
+    Decimal = 5,
+
+    /// <summary>
+    /// Gets double as property type.
+    /// </summary>
+    Double = 6,
+
+    /// <summary>
+    /// Gets the guid as property type.
+    /// </summary>
+    Guid = 7,
+
+    /// <summary>
+    /// Gets the style class as property type.
+    /// </summary>
+    StyleClass = 20,
+
+    /// <summary>
+    /// Gets the style rule as property type.
+    /// </summary>
+    StyleRule = 21,
+
+    /// <summary>
+    /// Gets the block attribute as property type.
+    /// </summary>
+    BlockAttribute = 30,
+
+    /// <summary>
+    /// Gets the enum as property type.
+    /// </summary>
+    Enum = 40,
+}
